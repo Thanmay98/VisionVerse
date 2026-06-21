40ba8c7ba1ea0d51c43eb29e29094fca1033085b
Thanmay Reddy <thanmayreddy0619@gmail.com>
2026-06-21 08:55:30 +0530

    Summary: Implemented scene switching and navigation flow

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScenarioSelection()
+    {
+        SceneManager.LoadScene("scenarioselection");
+    }
+
+    public void LoadEyeConditionSelection()
+    {
+        SceneManager.LoadScene("eyeconditionselection");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("main menu");
+    }
+}
